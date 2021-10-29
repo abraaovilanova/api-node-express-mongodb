caad4dd5bba44831aa1d0d8f7597946d591ee4f7
Abraão Vila Nova <abraaovilanova@gmail.com>
2021-10-29 12:53:38 -0300

    Autenticação

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 
 require('./controllers/authController')(app)
+require('./controllers/projectController')(app)
 
 app.listen(3001, ()=>{
     console.log('The server is running in port 3001')
